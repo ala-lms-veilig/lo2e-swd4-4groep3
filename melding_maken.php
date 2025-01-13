e10caeca66adb1450cabde96377763fe0fd0fe64
ElvinDavid <118258106+ElvinDavid@users.noreply.github.com>
2025-01-13 11:34:21 +0100

    pagina´s met elkaar verbonden

--- melding_maken.php
+++ melding_maken.php
@@ -198,7 +198,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         <nav>
             <div class="logo">mborijn/land</div>
             <ul class="nav-links">
-            <li><a href="index.php">HOME</a></li>
+            <li><a href="index.html">HOME</a></li>
             <li><a href="informatie.html">INFORMATIE</a></li>
             <li><a href="contact.html">CONTACT</a></li>
             <li><a href="melding_maken.php">MELDING MAKEN</a></li>
